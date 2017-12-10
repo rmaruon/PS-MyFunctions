152f9b378f52e50e50b5dc88e43afe628c77a2d8
rmaruon <rmaruon@gmail.com>
2017-12-10 22:36:47 +0900

    Add Remove-WordDocInfo

---
+++ Remove-WordDocInfo/Remove-WordDocInfo.ps1
@@ -0,0 +1,47 @@
+﻿function Remove-WordDocInfo {
+    <#
+    .SYNOPSIS
+        Delete all document informations of Word file(.doc, .docx).
+
+    .DESCRIPTION
+
+    .EXAMPLE
+        Remove-WordDocInfo input.docx
+
+    .EXAMPLE
+        (Get-ChildItem *docx) | Remove-WordDocInfo
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        [string]
+        $FileName
+    )
+
+    begin {
+        Add-Type -AssemblyName Microsoft.Office.Interop.Word
+        $WdRemoveDocType = "Microsoft.Office.Interop.Word.WdRemoveDocInfoType" -as [type]
+    }
+
+    process {
+        $FileWORD = Convert-Path $FileName
+        $FileWORD_ext = (Get-ChildItem $FileWORD).Extension
+
+        if ($FileWORD_ext -inotmatch "\.doc[x]?$") {
+            Write-Host $FileWORD ": You can remove document informations of .doc or .docx"
+        }
+        else {
+            $word = New-Object -ComObject WORD.APPLICATION
+            $word.visible = $false
+
+            $documents = $word.Documents.Open($FileWORD)
+            $documents.RemoveDocumentInformation($WdRemoveDocType::wdRDIAll)
+            $documents.Save()
+
+            $word.Quit()
+        }
+    }
+
+    end {
+    }
+}
