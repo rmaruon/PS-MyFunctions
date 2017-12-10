65beaa1b4627eb1998edf9f12a9cc38d81ffa73a
rmaruon <rmaruon@gmail.com>
2017-12-10 20:29:18 +0900

    Add Remove-ExcelDocInfo

---
+++ Remove-ExcelDocInfo/Remove-ExcelDocInfo.ps1
@@ -0,0 +1,48 @@
+﻿function Remove-ExcelDocInfo {
+    <#
+    .SYNOPSIS
+        Delete all document information of Excel file(.xls, .xlsm, .xlsx).
+
+    .DESCRIPTION
+
+    .EXAMPLE
+        Remove-ExcelDocInfo
+
+    .EXAMPLE
+        (Get-ChildItem *xlsx) | Remove-ExcelDocInfo
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline = $true, Mandatory = $true)]
+        [string]
+        $FileName
+    )
+
+    begin {
+        Add-Type -AssemblyName Microsoft.Office.Interop.Excel
+        $xlRemoveDocType = "Microsoft.Office.Interop.Excel.XlRemoveDocInfoType" -as [type]
+    }
+
+    process {
+        $FileEXCEL = Convert-Path $FileName
+        $FileEXCEL_ext = (Get-ChildItem $FileEXCEL).Extension
+
+        if ($FileEXCEL_ext -inotmatch "\.xls[mx]?$") {
+            Write-Host $FileEXCEL ": You can convert .xls, .xlsm or .xlsx"
+        }
+        else {
+            $excel = New-Object -COMOBJECT EXCEL.APPLICATION
+            $excel.visible = $false
+
+            $workbook = $excel.workbooks.open($FileEXCEL)
+            $workbook.RemoveDocumentInformation($xlRemoveDocType::xlRDIAll)
+            $workbook.Save()
+
+            $excel.Workbooks.close()
+        }
+    }
+
+    end {
+        $excel.Quit()
+    }
+}
